51b48353f72e0634e68d6b0bb24de33c18f48cea
Rafael Rivera <rriveratechie@gmail.com>
2020-11-22 00:33:40 -0500

    first commit

---
+++ CSharpFundamentals/01_Types/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("01_Types")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_Types")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5b76851-666c-40f0-9a29-6abe4ad4ee6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharpFundamentals/01_Types/TypeExamples.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_Types
+{
+    [TestClass]
+    public class TypeExamples
+    {
+        [TestMethod]  //pasquel case ValueTypes
+        public void ValueTypes()
+        {
+            //   whole numbers
+            byte oneBytesWorth;
+            oneBytesWorth = 255;
+            short smallWholeNumnber = 16;
+            int wholeNumber = 32;
+            Int16 anotherSmallNumber = 16; 
+            long largeWholeNumber = 64;
+
+            //--Decimals
+            float floatExamples = 1.234567f;
+            double doubleExample = 1.237909;
+            decimal decimalExample = 1.700000m;
+
+            //Character
+            char letter = 'R';
+            char reTurn = '\n';
+
+        }
+    }
+}
