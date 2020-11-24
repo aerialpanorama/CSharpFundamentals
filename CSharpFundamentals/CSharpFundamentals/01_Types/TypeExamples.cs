d30e2995351e605321e4c68d0bfc2d9b4cde93cb
aerialpanorama <rriveratechie@gmail.com>
2020-11-23 23:06:34 -0500

    Fourth Commit

--- CSharpFundamentals/CSharpFundamentals/01_Types/TypeExamples.cs
+++ CSharpFundamentals/CSharpFundamentals/01_Types/TypeExamples.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace _01_Types
@@ -42,6 +43,68 @@ namespace _01_Types
             Console.WriteLine(remainder);
 
             Console.WriteLine("Hello");
+
+
+
+        }
+        [TestMethod]
+        public void ReferenceTypes()
+        {
+            // Strings
+            string stringExample = "This is a string.";
+            string name = "Rafael";
+
+            string declared;
+            declared = "Now it;'s initialized.";
+
+            // Formatting strings
+            string concatenate = stringExample + " " + name; //This is a string. Rafael
+            string interpolate = $"{name}. here is the string: {stringExample}."; //Rafael. here is a string: This is a string.
+            Console.WriteLine(concatenate);
+            Console.WriteLine(interpolate);
+
+            //-- Other Object Examples
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            DateTime randomDate = new DateTime(1975, 4, 29); //() parenthesis is the constructor - new is a keyword
+            
+            TimeSpan waitTime = randomDate - now; // used to find difference between time (time Interval)
+                            
+            //dot operator and property below
+            Console.WriteLine(waitTime.TotalSeconds);
+
+            //--Collections are also reference types
+            // Arrays -not normally used in C Sharp
+            string anotherExampleString = "Hello World"; // this is a string
+            string[] stringArray = { anotherExampleString, "Hello", "World", "!" }; // square brackets defines an array
+            string[] students = new string[15];
+
+            Console.WriteLine(students); //outputs stringArray[]
+
+            // Lists
+            List<string> listOfStrings = new List<string>();
+            listOfStrings.Add("Lawrence"); // no predetermined size //collection of similar types. normally how arrays work in other languages
+
+
+            //Quees
+            Queue<string> firstInFirstOut = new Queue<string>();
+            firstInFirstOut.Enqueue("Luke");
+
+            //Dictionaries have to give key and value
+                        //key //value    
+            Dictionary<char, string> keyValuePair = new Dictionary<char, string>();
+            keyValuePair.Add('j', "Josh"); //use J to get value of josh - (key&door)
+
+            //Other Collections
+            SortedList<int, string> sortedKeyValuePair = new SortedList<int, string>();
+            Stack<string> firstInLastOut = new Stack<string>();
+
+            
+
+           
+
 
 
 
